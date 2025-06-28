c6944e7109b0a06427ab14148ba2ce371feefbb1
MatteoGabr <matteo.gabrielli@edu.itspiemonte.it>
2025-06-28 11:12:40 +0200

    App PremierLeague

---
+++ football/src/main/java/com/football/entities/Player.java
@@ -0,0 +1,43 @@
+package com.football.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="prem_stats")
+public class Player {
+
+    @Id
+    @Column(name="name", unique=true)
+    private String name;
+
+    private String nation;
+
+    private String pos; 
+
+    private Integer age;
+
+    private Integer mp;
+
+    private Integer starts;
+
+    private Double min;
+
+    private Double gls;
+
+    private Double ast;
+
+    private Double pk;
+
+    private Double crdy;
+
+    private Double crdr;
+
+    private Double xg;
+
+    private Double xag;
+
+    private String team;
+}
